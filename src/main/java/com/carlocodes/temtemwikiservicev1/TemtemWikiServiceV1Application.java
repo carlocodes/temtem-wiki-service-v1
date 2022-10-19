7911832eb0b4d6fbdfb7f32ff2b33562a38e6feb
carlocodes <Gian Uy>
2022-10-19 19:29:05 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/temtemwikiservicev1/TemtemWikiServiceV1Application.java
@@ -0,0 +1,13 @@
+package com.carlocodes.temtemwikiservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemtemWikiServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemtemWikiServiceV1Application.class, args);
+	}
+
+}
